0c0ca5529a767c9f232e8d6bfc4b183255551817
Brad Aagaard <baagaard@usgs.gov>
2005-07-23 00:27:33 +0000

    original source.

---
+++ pkgs/average/Make.mm
@@ -0,0 +1,39 @@
+# -*- Makefile -*-
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# <LicenseText>
+#
+# ----------------------------------------------------------------------
+#
+
+PROJECT = cencalvm
+PACKAGE = average
+
+RECURSE_DIRS = \
+	lib \
+	driver \
+
+OTHERS = \
+
+# ----------------------------------------------------------------------
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+export::
+	BLD_ACTION="export" $(MM) recurse
+
+# version
+# $Id$
+
+# End of file 
