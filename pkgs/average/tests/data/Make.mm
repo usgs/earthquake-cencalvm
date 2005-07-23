d49759eb542c86b52aec818bcb7c9122cce536fa
Brad Aagaard <baagaard@usgs.gov>
2005-07-23 23:20:32 +0000

    original source

---
+++ pkgs/average/tests/data/Make.mm
@@ -0,0 +1,25 @@
+# -*- Makefile -*-
+#
+# ----------------------------------------------------------------------
+#
+#                           Brad T. Aagaard
+#                        U.S. Geological Survey
+#
+# {LicenseText}
+#
+# ----------------------------------------------------------------------
+#
+
+PROJECT = cencalvm
+PACKAGE = average/tests/data
+
+PROJ_CLEAN += in.etree out.etree
+
+default all:
+
+# ----------------------------------------------------------------------
+
+# version
+# $Id$
+
+# End of file 
