5c62567d975f2b3f4b3132c54b14147b068b8e65
Brad Aagaard <baagaard@usgs.gov>
2005-07-25 15:08:05 +0000

    Created targets for pre1.0 and 1.0.0 tests.

--- pkgs/query/examples/cpp/Make.mm
+++ pkgs/query/examples/cpp/Make.mm
@@ -21,7 +21,9 @@ PROJ_SRCS = \
 
 PROJ_BIN = $(PROJ_BINDIR)/cencalvmcppquery
 
-TEST_ARGS = -d data/cencalvm-pre1.0.etree -i ../data/sample.in -o test.out -l test.log
+TEST_PRE1.0_ARGS = -d data/ver-pre1.0/cencalvm-pre1.0.etree -i ../data/sample-pre1.0.in -o test.out -l test.log
+
+TEST_1.0.0_ARGS = -d data/ver-1.0.0/cencalvm-1.0.0.etree -i ../data/sample-1.0.0.in -o test.out -l test.log
 
 # ----------------------------------------------------------------------
 all: $(PROJ_BIN)
@@ -29,8 +31,11 @@ all: $(PROJ_BIN)
 $(PROJ_BIN):: product_dirs $(PROJ_OBJS)
 	$(CXX) -o $@ $(LCXXFLAGS) $(PROJ_OBJS)
 
-test:
-	@LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(BLD_LIBDIR) $(PROJ_BIN) $(TEST_ARGS)
+test-pre1.0:
+	@LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(BLD_LIBDIR) $(PROJ_BIN) $(TEST_PRE1.0_ARGS)
+
+test-1.0.0:
+	@LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(BLD_LIBDIR) $(PROJ_BIN) $(TEST_1.0.0_ARGS)
 
 export:: export-binaries
 
