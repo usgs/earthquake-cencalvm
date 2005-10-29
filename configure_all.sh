0d42d0d918e3c8d178bda8b9782737700439e9ef
Brad Aagaard <baagaard@usgs.gov>
2005-10-29 05:55:39 +0000

    Replaced by swconfig.py.

--- configure_all.sh
+++
@@ -1,6 +0,0 @@
-../src/configure --prefix=/home/tools/cencalvm \
-    --enable-testing \
-    --enable-documentation \
-    --config-cache \
-    CPPFLAGS="-I${ETREE_INCDIR} -I${PROJ4_INCDIR} -I${CPPUNIT_INCDIR}" \
-    LDFLAGS="-L${ETREE_LIBDIR} -L${PROJ4_LIBDIR} -L${CPPUNIT_LIBDIR}" \
