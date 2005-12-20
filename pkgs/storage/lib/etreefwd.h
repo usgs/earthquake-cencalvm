7986fc23b6a3b634745e9b7cc2223b1c8d2ce7cd
Brad Aagaard <baagaard@usgs.gov>
2005-12-20 01:21:10 +0000

    Fixed typedef definitions.

--- pkgs/storage/lib/etreefwd.h
+++ pkgs/storage/lib/etreefwd.h
@@ -20,8 +20,8 @@
 
 #include <inttypes.h> // USES uint32_t
 
-typedef struct etree_addr_t; ///< etree address
-typedef struct etree_t; ///< etree database
+typedef struct etree_addr_t etree_addr_t; ///< etree address
+typedef struct etree_t etree_t; ///< etree database
 typedef uint32_t etree_tick_t; ///< etree coordinate
 
 #endif // cencalvm_storage_etreefwd_h
